5b16916d32cfdb23d80c58f2549f3d4f186dcab0
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-10-15 19:44:55 +0100

    Created GameSession and included Player class

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+        public string Name {  get; set; }
+        public string Job {  get; set; }
+        public int HP {  get; set; }
+        public int EXP {  get; set; }
+        public int Gold { get; set; }
+        public int Level { get; set; }
+
+
+
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,26 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new();
+            CurrentPlayer.Name = "Player One";
+            CurrentPlayer.Gold = 5000;
+            CurrentPlayer.EXP = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.HP = 100;
+            CurrentPlayer.Job = "Mage";
+        }
+    }
+}
